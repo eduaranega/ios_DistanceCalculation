8121945c6cf769f1b07ad26d3c53aac30bf1a432
Eduardo Aranega <aranega@yahoo-inc.com>
2016-05-03 21:34:30 -0700

    UC Irvine iOS pre-assignment 4

---
+++ DistanceCalculation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DistanceCalculation
+//
+//  Created by Eduardo Aranega on 5/3/16.
+//  Copyright © 2016 Eduardo Aranega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
