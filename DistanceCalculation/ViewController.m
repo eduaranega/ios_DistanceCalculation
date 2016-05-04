74f1f764395ca2a2ccf0e821ab5346b9524bf004
Eduardo Aranega <aranega@yahoo-inc.com>
2016-05-03 22:19:16 -0700

    Added properties and initial button implementation

--- DistanceCalculation/ViewController.m
+++ DistanceCalculation/ViewController.m
@@ -7,21 +7,55 @@
 //
 
 #import "ViewController.h"
+#import <DistanceGetter/DGDistanceRequest.h>
 
 @interface ViewController ()
+
+@property (nonatomic) DGDistanceRequest *request;
+
+@property (weak, nonatomic) IBOutlet UITextField *startLocation;
+
+@property (weak, nonatomic) IBOutlet UITextField *endLocationA;
+@property (weak, nonatomic) IBOutlet UILabel *distanceA;
+
+@property (weak, nonatomic) IBOutlet UITextField *endLocationB;
+@property (weak, nonatomic) IBOutlet UILabel *distanceB;
+
+@property (weak, nonatomic) IBOutlet UITextField *endLocationC;
+@property (weak, nonatomic) IBOutlet UILabel *distanceC;
+
+@property (weak, nonatomic) IBOutlet UIButton *calculateButton;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)calculateButtonTapped:(id)sender {
+
+    self.calculateButton.enabled = NO;
+
+    self.request = [DGDistanceRequest alloc];
+    NSString *start = self.startLocation.text;
+    NSString *destA = self.endLocationA.text;
+    NSString *destB = self.endLocationB.text;
+    NSString *destC = self.endLocationC.text;
+
+    NSArray *destinations = @[destA,destB,destC];
+
+    self.request = [self.request initWithLocationDescriptions:destinations
+                                            sourceDescription:start];
+
+    // define the callback
+    self.request.callback = ^void(NSArray *responses) {
+        self.distanceC.text = @"callback";
+        self.calculateButton.enabled = YES;
+        self.request = nil;
+    };
+
+    // send a start messega to the object
+    [self.request start];
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
